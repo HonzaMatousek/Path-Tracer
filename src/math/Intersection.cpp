e21393780d6c30b1c05f5d0a4323bf54cd7609fa
Jan Matoušek <jan.matousek@rekomando.cz>
2020-07-19 20:23:39 +0200

    Implement Fresnel equations.

--- src/math/Intersection.cpp
+++ src/math/Intersection.cpp
@@ -29,43 +29,70 @@ Vector3D RandomDirection(const Vector3D & usualDirection, std::mt19937 & generat
 Ray Intersection::Reflect(const Ray &incoming, double& powerMultiplier, double& refractiveIndex, std::mt19937 & generator) {
     auto material = GetMaterial();
     auto normal = RandomDirection(GetNormal(material.normal), generator, material.roughness);
+
+    // fresnel equation
+    // Rs = pow(n1 cos phii - n2 cos phit / n1 cos phii + n2 cos phit, 2) // pricna polarizace
+    // Rp = pow(n1 cos phit - n2 cos phii / n1 cos phit + n2 cos phii, 2) // podelna polarizaci
+
+    double n1 = refractiveIndex;
+    double cos_i = normal.Dot(incoming.direction); // cosi
+    double sin_i = sqrt(1 - cos_i * cos_i);
+    double refraction_ratio;
+    if(cos_i > 0) {
+        // going from inside out
+        refraction_ratio = material.refractiveIndex;
+        refractiveIndex = 1;
+        normal *= -1;
+        powerMultiplier *= pow((1 - material.opacity), t);
+    }
+    else {
+        // going from outside in
+        refraction_ratio = refractiveIndex / material.refractiveIndex;
+        refractiveIndex = material.refractiveIndex;
+        cos_i *= -1;
+    }
+    double sini = sqrt(1 - cos_i * cos_i);
+    if(sini > 1 / refraction_ratio) {
+        refractiveIndex = material.refractiveIndex;
+        Vector3D idealReflection = incoming.direction - normal * (2 * incoming.direction.Dot(normal));
+        return Ray(incoming.Point(t - 0.00000001), idealReflection);
+    }
+    double sin_t = refraction_ratio * sini;
+    double cos_t = sqrt(1 - sin_t * sin_t);
+    double n2 = refractiveIndex;
+
+    double rs = pow((n1 * cos_i - n2 * cos_t) / (n1 * cos_i + n2 * cos_t), 2); // reflection in s mode
+    double rp = pow((n1 * cos_t - n2 * cos_i) / (n1 * cos_t + n2 * cos_i), 2); // reflection in p mode
+
+    double reflection = (rs + rp) / 2;
+    double transmission = 1 - reflection;
+
     if(material.metalness) {
         //Vector3D normal = RandomDirection(normal, generator, 0.0);
+        powerMultiplier *= reflection;
         Vector3D idealReflection = incoming.direction - normal * (2 * incoming.direction.Dot(normal));
         return Ray(incoming.Point(t - 0.00000001), idealReflection);
     }
     else {
-        if(d(generator) >= material.opacity) {
-            // refract
-            double ni = normal.Dot(incoming.direction); // cosi
-            double mi;
-            if(ni > 0) {
-                // going from inside out
-                mi = material.refractiveIndex;
-                refractiveIndex = 1;
-                normal *= -1;
-                powerMultiplier *= pow((1 - material.opacity), t);
-            }
-            else {
-                // going from outside in
-                mi = refractiveIndex / material.refractiveIndex;
-                refractiveIndex = material.refractiveIndex;
-                ni *= -1;
-            }
-            double sini = sqrt(1 - ni*ni);
-            if(sini > 1 / mi) {
-                refractiveIndex = material.refractiveIndex;
-                Vector3D idealReflection = incoming.direction - normal * (2 * incoming.direction.Dot(normal));
-                return Ray(incoming.Point(t - 0.00000001), idealReflection);
-            }
-            double sint = mi * sini;
-            Vector3D refracted = normal * -1 * sqrt(1 - sint * sint) + normal.Cross(incoming.direction).Cross(normal).Normalize() * sint;
-            return Ray(incoming.Point(t + 0.00000001), refracted);
+        if(d(generator) <= reflection) {
+            // reflection
+            powerMultiplier *= reflection;
+            Vector3D idealReflection = incoming.direction - normal * (2 * incoming.direction.Dot(normal));
+            return Ray(incoming.Point(t - 0.00000001), idealReflection);
         }
         else {
-            auto randomDirection = RandomDirection(normal, generator, 1.0);
-            powerMultiplier *= randomDirection.Dot(normal);
-            return Ray(incoming.Point(t - 0.00000001), randomDirection);
+            // transmission
+            if(d(generator) >= material.opacity) {
+                // refract
+                Vector3D refracted = normal * -1 * sqrt(1 - sin_t * sin_t) + normal.Cross(incoming.direction).Cross(normal).Normalize() * sin_t;
+                return Ray(incoming.Point(t + 0.00000001), refracted);
+            }
+            else {
+                // diffuse
+                auto randomDirection = RandomDirection(normal, generator, 1.0);
+                powerMultiplier *= randomDirection.Dot(normal);
+                return Ray(incoming.Point(t - 0.00000001), randomDirection);
+            }
         }
     }
 }
